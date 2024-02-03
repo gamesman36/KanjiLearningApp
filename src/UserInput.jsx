087eef3548d497dc68e2730b0e380d59c454a9ef
Daniel Andreas Wang <gamesman36@outlook.com>
2024-02-03 08:56:17 +0100

    Expanding form + renaming JSON fields

--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,18 +1,29 @@
 import { useRef } from "react";
 
 function UserInput() {
-    const inputRef = useRef();
+    const onyomiRef = useRef();
+    const kunyomiRef = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const answer = inputRef.current.value;
-        alert(`${answer}`);
+        const onyomi = onyomiRef.current.value;
+        const kunyomi = kunyomiRef.current.value;
+
+        processAnswers(onyomi, kunyomi);
+
+        onyomiRef.current.value = "";
+        kunyomiRef.current.value = "";
+    }
+
+    const processAnswers = (onyomi, kunyomi) => {
+        alert(`Onyomi: ${onyomi} Kunyomi: ${kunyomi}`);
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input ref={inputRef} type="text" placeholder="answer" />
+                    <input ref={onyomiRef} type="text" placeholder="Onyomi" /><br />
+                    <input ref={kunyomiRef} type="text" placeholder="Kunyomi" /><br />
                 <button type="submit">Submit</button>
             </form>
         </div>
